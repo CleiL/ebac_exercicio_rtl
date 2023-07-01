037b38182759aabe84616b01adecf2e245bd60c2
CleiL <clei.lisboa@gmail.com>
2023-07-01 09:59:07 -0300

    atualizado

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -4,8 +4,8 @@ import Post from '.';
 describe('Test <Post />', () => {
     it('Expect render correctly', () => {
         render(
-            <Post imageUrl="https://via.placeholder.com/300x300">
-                Teste
+            <Post imageUrl="https://th.bing.com/th?id=OIP.1m052NUg8zmJMnSf5M9X0AHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2">
+                Testes
             </Post>
         );
         expect(screen.getByText('Teste')).toBeInTheDocument();
